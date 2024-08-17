31dbd01d71d848bdc16a373f121076432c0213de
0xabin <abinbvinod@gmail.com>
2024-08-17 17:58:16 +0530

    Faucet init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { zeroAddress } from "viem";
 import { Mnemonicaccount } from "./helpers/account";
-import { mevmRequestFaucet } from "./helpers/getGasreq";
 import { asciiConsole } from "./util/asciiConsole";
 import { URlCHAIN } from "./util/chains";
 import { getBalance } from "./helpers/getBalance";
@@ -13,18 +12,17 @@ console.log("Wallet Address:", Mnemonicaccount.address);
 console.log("\n"); 
 
 
-const BotRun = async () => {
+const asyncFunc = async () => {
     try {
-    //   await mevmRequestFaucet(URlCHAIN.mevmM1.url, Mnemonicaccount.address, "6LdPgxMqAAAAAByFdD5V8PiPKYZS4mSZWUUcZW6B");
       const balance = await getBalance(Mnemonicaccount.address);
-    //   console.log("Balance after faucet request:", balance);
+
     } catch (error) {
       console.error("Error in async function:", error);
     }
   };
   
-
-  BotRun();
+  // Call the async function
+  asyncFunc();
 
 
 
